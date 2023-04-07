5afd557121aa90f9a368fa5099f2f5a643a58ddb
Krisna Pranav <krisna.pranav@gmail.com>
2023-04-07 14:42:53 +0530

    frontend: loginpage[added some functionalities]

--- frontend/loginpage/login.js
+++ frontend/loginpage/login.js
@@ -1,3 +1,16 @@
+const http = require('http'); // http library which you need.
 
+function getLogin(err, token) {
+    const userResp = ''; // url
+    console.log(userResp);
 
+    if (userResp === true) {
+        // user functionalities
+    } else {
+        throw(err)
+    }
+}
 
+function saveTokenHeaders(token) {
+    getLogin.userResp['token'];
+}
